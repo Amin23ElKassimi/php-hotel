2192d17c58e8357abac197e0078e82716e66103f
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-01-15 22:37:33 +0100

    change

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $hotels = [
@@ -47,18 +46,39 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotel</title>
+    <!-- Collegamenti a Bootstrap -->
+    <link rel="stylesheet" href="path/to/bootstrap/css/bootstrap.min.css">
+    <script src="path/to/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<ul>
-    <?php foreach ( $hotels as $hotel ) { ?>
-        <li>
-            <pre>
-                <?php echo   ' - Name: ' .$hotel['name'] . ' - Description: ' . $hotel['description']. ' - Parking: ' . $hotel['parking'] . ' - Vote: ' . $hotel['vote'] . ' - Distance to center: ' . $hotel['distance_to_center']. ' Km'; ?>
-            </pre>
-        </li>
-    <?php } ?>
-</ul>
-    
+<div class="container mt-5">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Parking</th>
+                <th>Vote</th>
+                <th>Distance to Center (Km)</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            <?php foreach ($hotels as $hotel) { ?>
+                <tr>
+                    <td><?php echo $hotel['name']; ?></td>
+                    <td><?php echo $hotel['description']; ?></td>
+                    <td><?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></td>
+                    <td><?php echo $hotel['vote']; ?></td>
+                    <td><?php echo $hotel['distance_to_center']; ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+
+<!-- Opzionale: Bootstrap richiede jQuery, quindi assicurati di includerlo prima di Bootstrap JS -->
+<script src="path/to/jquery/jquery.min.js"></script>
+
 </body>
 </html>
