44ff35c4ca96b292c4d5acc9f20afb2f6cc09e06
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-01-16 00:01:07 +0100

    link bootstrap working

--- index.php
+++ index.php
@@ -47,14 +47,15 @@ $hotels = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotel</title>
     <!-- Collegamenti a Bootstrap -->
-    <link rel="stylesheet" href="path/to/bootstrap/css/bootstrap.min.css">
+    <!-- CSS Link Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="path/to/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
 
 <div class="container mt-5">
     <table class="table">
-        <thead>
+        <thead class="text-primary">
             <tr>
                 <th>Name</th>
                 <th>Description</th>
